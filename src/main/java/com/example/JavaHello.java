6b9bc684751af84cf922478f1c9b9ff82ba05b43
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2016-06-05 23:32:46 +0900

    initial commit

---
+++ src/main/java/com/example/JavaHello.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @author yu1ro
+ */
+@SuppressWarnings("All")
+public class JavaHello {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.toUpperCase());
+    }
+}
