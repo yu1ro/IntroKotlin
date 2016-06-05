e3404b8c1fe3503a2c797e3584a15ac38740e5bb
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2016-06-06 00:21:32 +0900

    compile error sample

--- src/main/java/com/example/JavaHello.java
+++ src/main/java/com/example/JavaHello.java
@@ -3,7 +3,7 @@ package com.example;
 /**
  * @author yu1ro
  */
-@SuppressWarnings("All")
+@SuppressWarnings("ALL")
 public class JavaHello {
     public static void main(String[] args) {
         String str = null;
---
+++ src/main/java/com/example/KotlinHello.kt
@@ -0,0 +1,6 @@
+package com.example
+
+fun main(args: Array<String>) {
+    val str: String = null;
+    print(str.toUpperCase())
+}
